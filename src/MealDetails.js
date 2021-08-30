64fb1f46857675e7c2b3c30fc3cc67f3d6e84f7c
Begimai Agybaeva <begimai.a@yandex.ru>
2021-08-30 09:16:18 +0600

    added video

--- src/MealDetails.js
+++ src/MealDetails.js
@@ -5,7 +5,7 @@ import "./meal.css"
 
 const MealDetails = () => {
     const [meal, setMeal] = useState({})
-
+    const [youtube, setYoutube] = useState("")
     const [ingred, setIngred] = useState([])
     const params = useParams()
 
@@ -18,40 +18,47 @@ const MealDetails = () => {
                         return [...acc, obj[`strIngredient${idx + 1}`]]
                     }
                     return acc
-
                 }, [params.id])
-
                 setIngred(strIng)
                 setMeal(obj)
+                setYoutube(obj.strYoutube.slice(obj.strYoutube.indexOf("v=") + 2, obj.strYoutube.lenght))
             })
-
     }, [params.id])
 
     return (
         <div>
-        <div className="row">
-          <div className="col-6">
-              <p className="meal-title">{meal.strMeal}</p>
-              <img src={meal.strMealThumb}  alt="" className="meal-img"/>
-          </div>
-            <div className="col-6">
-                <div className="row">
-                    {
-                        ingred.map((item, idx) => (
-                            <div key={idx} className="col-3 ing-wrapper">
-                                <Link to={`/ingredients/${item}`}>
-                                    <div>{item}</div>
-                                    <img src={`https://www.themealdb.com/images/ingredients/${item}.png`} alt=""/>
-                                </Link>
-                            </div>
-                        ))
-                    }
+            <div className="row">
+                <div className="col-6">
+                    <p className="meal-title">{meal.strMeal}</p>
+                    <img src={meal.strMealThumb} alt="" className="meal-img"/>
                 </div>
+                <div className="col-6">
+                    <div className="row">
+                        {
+                            ingred.map((item, idx) => (
+                                <div key={idx} className="col-3 ing-wrapper">
+                                    <Link to={`/ingredients/${item}`}>
+                                        <div>{item}</div>
+                                        <img src={`https://www.themealdb.com/images/ingredients/${item}.png`} alt=""/>
+                                    </Link>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="col-6">
+                    <div><h4> Instructions:</h4> {meal.strInstructions}</div>
+                </div>
+                {
+                    youtube.length !== 0 &&
+                    <iframe width="560" height="315" src={`https://www.youtube.com/embed/${youtube}`}
+                            title="YouTube video player" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen></iframe>
+                }
+
+
             </div>
-            <div className="col-6">
-                <div> <h4> Instructions:</h4> {meal.strInstructions}</div>
-            </div>
-        </div>
 
         </div>
     );
