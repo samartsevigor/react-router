807143d34e822409d51fba8961eb26ae236cd3a9
Begimai Agybaeva <begimai.a@yandex.ru>
2021-09-01 08:48:26 +0600

    update path app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
-
+import Header from "./components/Header/Header";
+import Home from "./views/Home/Home";
+import Meals from "./views/Meals/Meals";
 import MealDetails from "./views/MealDetails/MealDetails";
 import Browse from "./views/Browse/Browse";
-import Search from "./components/Search/Search";
-import Home from "./views/Home/Home";
-import Header from "./components/Header/Header";
 import Ingredients from "./views/Ingredients/Ingredients";
-import Meals from "./views/Meals/Meals";
+
 
 function App() {
   return (
@@ -15,11 +14,11 @@ function App() {
     <div className="container">
         <Router>
             <Header />
-            <Route exact path="/" > <Home/> </Route>
-            <Route exact path="/meals" ><Search /> <Meals /> </Route>
-            <Route path="/meals/:id" > <MealDetails /></Route>
-            <Route path="/browse/:name"> <Browse /></Route>
-            <Route path="/ingredients/:name" > <Ingredients/> </Route>
+            <Route exact path="/" > <Home /> </Route>
+            <Route exact path="/meals" ><Meals /> </Route>
+            <Route path="/meals/:id" ><MealDetails /></Route>
+            <Route path="/browse/:name"><Browse /></Route>
+            <Route path="/ingredients/:name" ><Ingredients /></Route>
 
         </Router>
     </div>
